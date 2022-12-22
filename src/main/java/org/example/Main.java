5463da918be9359db9771a090b22ebc0f528657d
istrebitelantox <geekpook@gmail.com>
2022-12-22 17:31:45 +0300

    Arrays calculating complete

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,25 +1,36 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        String love = "Я люблю програмирование";
-        String hello="Привет";
-        String name="Юра";
-        System.out.println("Изначальные строки :\n\t"+hello+"\n\t"+love+"\n\t"+name);
-        if(hello.equals("Пpивет"))
-            System.out.println("Неверно!");
-        else
-            System.out.println("Верно!");
-        char k=hello.charAt(3);
-        String []loves=love.split(" ");
-        for(String loveit:loves)
-            System.out.println("Разбиение строки : "+loveit);
-        System.out.println("Количество символов : "+hello.length());
-        System.out.println("Вывод подстроки : "+love.substring(8));
-        System.out.println("Соединение строк : "+hello+", "+name);
-        System.out.println("Четвёртый символ в строке : "+k);
-        System.out.println("Перевод в нижний регистр : "+hello.toLowerCase());
-        System.out.println("Перевод в верхний регистр : "+hello.toUpperCase());
-        System.out.println("Замена слова в строке : "+love.replace("люблю","ненавижу"));
+        double []ser=new double[5];
+        for(int i=0;i<5;i++)
+        {
+            ser[i]=Math.random()*100;
+        }
+        //int []ser=new int[6];
+        int summa=0;
+        double max=0;
+        double min=ser[0];
+        long multi=1;
+        for(int i=0;i<ser.length;i++)
+        {
+            if (max<ser[i])
+                max=ser[i];
+            if(min>ser[i])
+                min=ser[i];
+            System.out.println("Переменная номер "+i+" = "+ ser[i]);
+            summa+=ser[i];
+            multi*=ser[i];
+        }
+        System.out.println("Сумма ( способ #1) = "+summa);
+        System.out.println("Сумма ( способ #2) = "+ Arrays.stream(ser).sum());
+        System.out.println("Умножение = "+multi);
+        System.out.println("Самое большое число в массиве( способ #1) = "+max);
+        System.out.println("Самое большое число в массиве( способ #2) = "+ Arrays.stream(ser).max());
+        System.out.println("Самое маленькое число в массиве( способ #1) = "+ Arrays.stream(ser).min());
+        System.out.println("Самое маленькое число в массиве( способ #2) = "+min);
+
     }
 }
